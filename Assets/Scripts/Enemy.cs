47e9770350345b3b417b26568f2a54c822519e40
Juan Valen <al.valen@hotmail.com>
2022-04-04 13:11:16 +0300

    Added new enemy and projectile prefabs and scripts. Made a simple background for temporary use and gave it a color.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,18 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//In order for the script to work correctly, enemy object requires an additional box collider thar is marked as trigger and functions as the attack range
+//In order for the script to work correctly, enemy object requires an additional collider thar is marked as trigger and functions as the attack range
 //Both enemy and player objects have rotation constraints in order to avoid them from falling on collision
 public class Enemy : MonoBehaviour
 {
     //variables
     public float speed = 5.0f;
+
     private GameObject player;
     private Rigidbody enemyRb;
     public bool playerInRange;
-    public GameObject projetilePrefab;
-    public float projectileDelay = 5;
-    public float projectileTimer;
+    public GameObject projectilePrefab;
+    public Transform target;
+
+    private Vector3 offset = new Vector3(.1f, 1, 0);
+
+    private float projectileDelay = 100;
+    private float projectileTimer = Time.time;
+
     void Start()
     {
         enemyRb = GetComponent<Rigidbody>();
@@ -25,10 +31,17 @@ public class Enemy : MonoBehaviour
     {
         if (playerInRange && projectileTimer >= projectileDelay)
         {
-            Invoke("spawnProjectile", 2);
+            Invoke("spawnProjectile", 0);
+
             projectileTimer = 0;
+            
+            transform.LookAt(player.transform.position, Vector3.forward);
+            
+            var point = target.position;
+            point.y = transform.position.y;
+            transform.LookAt(point);
         }
-        
+
         if (projectileTimer < projectileDelay)
         {
             projectileTimer++;
@@ -38,7 +51,7 @@ public class Enemy : MonoBehaviour
     //Checks if player is colliding with enemy attack range
     void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.name == "Player")
+        if (other.gameObject.name == "Player")
         {
             playerInRange = true;
         }
@@ -46,17 +59,20 @@ public class Enemy : MonoBehaviour
 
     void OnTriggerExit(Collider other)
     {
-        if(other.gameObject.name == "Player")
+        if (other.gameObject.name == "Player")
         {
             playerInRange = false;
         }
     }
 
+
     void spawnProjectile()
     {
-        Instantiate(projetilePrefab, transform.position, projetilePrefab.transform.rotation);
+        Instantiate(projectilePrefab, transform.position + offset, transform.rotation);
+        projectileTimer = Time.deltaTime;
+        if (projectileTimer >= projectileDelay)
+        {
+            projectileTimer = 0;
+        }
     }
-
-
-
 }
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,15 +5,30 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     public float speed = 10.0f;
+
+    private float range = 10f;
+    private Vector3 startPos;
+    private float distance;
+    private Rigidbody playerRb;
+
+    private GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = transform.position;
+        playerRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
+        distance = Vector3.Distance(startPos, transform.position);
+        if( range < distance || distance < -range)
+        {
+            Destroy(gameObject);
+        }
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
