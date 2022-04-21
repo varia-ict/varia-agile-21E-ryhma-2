db62e7fd6e0c081c331855add6e45171627dfe1f
Shrimptrimp <kasperkukkonen08@gmail.com>
2022-04-21 10:15:30 +0300

    messing around with terrain a bit

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -4,21 +4,23 @@ using UnityEngine;
 
 
 
-
 public class FollowPlayer : MonoBehaviour
 
+ 
 
 {
     // Start is called before the first frame update
+    Vector3 offset = new Vector3(0, 2, -10);
     public GameObject player;
     void Start()
     {
+
     }
 
     // Update is called once per frame
     void Update()
     {
         //follows the player on an offset
-        transform.position = player.transform.position + new Vector3(0, 2, -10);
-    }
+        transform.position = player.transform.position + offset;
+}
 }
