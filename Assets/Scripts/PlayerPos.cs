51a1a4f08163e30413b4a73c2a6890cd9347eab7
abhi1988sharma <abhi1988.sharma@gmail.com>
2022-05-23 13:00:45 +0300

    Commented on checkpoint scripts

--- Assets/Scripts/PlayerPos.cs
+++ Assets/Scripts/PlayerPos.cs
@@ -8,14 +8,16 @@ public class PlayerPos : MonoBehaviour
     private GameMaster gm;
     void Start()
     {
+        // grabs a reffernce from the game master
         gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
         transform.position = gm.lastCheckPointPos;
 
     }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.N))
+        if (Input.GetKeyDown(KeyCode.N)) // need to put player dying condition here
         {
+            // loads scene from last checkpoint position
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 
         }
